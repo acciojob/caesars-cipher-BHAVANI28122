28631012b18992e789f60d6c1012c99dbe61aa01
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-03-19 19:41:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,33 +2,35 @@
 function ROT13(encryptedstring) {
 
 
-	const lookup = {
-		A: "N",
-		B: "O",
-		C:  "P",
-	    D: "Q",
-		E: "R",
-		F: "S",
-		G: "T",
-		H:  "U",
-		I: "V",
-		J: "W",
-		K:  "X",
-		L:  "Y",
-		M: "Z",
-		N: "A",
-		O: "B",
-		P: "C",
-		Q: "D",
-		R: "E",
-		S: "F",
-		T: "G",
-		U: "H",
-		V: "I",
-        W: "J",
-		X: "K",
-		Y: "L",
-		Z: "M"
+	let lookup = {
+		'A': 'N',
+		'B': 'O',
+		'C': 'P',
+		'D': 'Q',
+		'E': 'R',
+		'F': 'S',
+		'G': 'T',
+		'H': 'U',
+		'I': 'V',
+		'J': 'W',
+		'K': 'X',
+		'L': 'Y',
+		'M': 'Z',
+		'N': 'A',
+		'O': 'B',
+		'P': 'C',
+		'Q': 'D',
+		'R': 'E',
+		'S': 'F',
+		'T': 'G',
+		'U': 'H',
+		'V': 'I',
+		'W': 'J',
+		'X': 'K',
+		'Y': 'L',
+		'Z': 'M',
+		
+	
 	};
 	let encrypt = encryptedstring.split(" ");
 	let decryptstring =[];
